cab59d55ae81f93ace42c8805ae9d80d05ed2167
Malik H <malik.h@webdevpro.net>
2023-06-22 11:21:52 +0200

    useState

--- jour4-communication/App.js
+++ jour4-communication/App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Compteur from './composants/Compteur';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Compteur />
       <StatusBar style="auto" />
     </View>
   );
---
+++ jour4-communication/composants/Compteur.jsx
@@ -0,0 +1,35 @@
+import { StyleSheet, Text, View , Button } from 'react-native'
+import React , { useState } from 'react' // importer le hook useState
+
+export default function Compteur() {
+   // let a = 10 ;
+    const [a, setA] = useState(10) // remplace la variable a par le state a
+    const [text , setText] = useState("liker")
+    const [btnBg, setBtnBg] = useState("blue")
+
+    function btnSpecial(){
+        if(text === "liker"){
+            setA( a + 1 )
+            setText("déliker")
+            setBtnBg("red")
+        }else {
+            setA( a - 1 )
+            setText("liker")
+            setBtnBg("blue")
+        }
+    }
+  return (
+    <View>
+      <Text style={styles.titre}>Compteur </Text>
+      <Text>{a}</Text>
+      <Button title={'Augmenter de + 1'} onPress={ function() { setA( a + 1 ) }}/>
+      <Button title={'Augmenter de - 1'} onPress={ function() { setA( a - 1 ) }}/>
+      <Button title={'zero'} onPress={ function() { setA( 0 ) }}/>
+      <Button title={ text } color={btnBg} onPress={ btnSpecial }/>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    titre : { fontSize : 25 }
+})
