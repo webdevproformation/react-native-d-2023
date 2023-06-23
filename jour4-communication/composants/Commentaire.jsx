fe48a6c88d4aa14cabe3b9d4f0423fbb049f98b0
Malik H <malik.h@webdevpro.net>
2023-06-23 10:44:24 +0200

    fin info

--- jour4-communication/composants/Commentaire.jsx
+++ jour4-communication/composants/Commentaire.jsx
@@ -1,14 +1,23 @@
 import { StyleSheet, Text, View , TextInput , Button } from 'react-native'
-import React from 'react'
+import React , {useState} from 'react'
 export default function Commentaire({article, setArticle}) {
+    const [texte , setTexte] = useState("")
+    function submit(){
+        const cloneArticle = {...article};
+        cloneArticle.commentaires.push(texte)
+        setArticle(cloneArticle) // ajouter les commentaires 
+        setTexte("") // permet de vider le textarea du texte écrit dedans 
+    }
   return (
     <View>
       <Text style={{ fontSize : 18 }}>Ajouter un nouveau Commentaire</Text>
       <TextInput 
         placeholder='laisser un commentaire' 
         multiline={true} numberOfLines={4} 
-        style={styles.input}/>
-      <Button title={'ajouter'} onPress={function(){}} />
+        style={styles.input} 
+        onChangeText={function( texteSaisi ){ setTexte(texteSaisi) }} 
+        value={texte}/>
+      <Button title={'ajouter'} onPress={submit} />
     </View>
   )
 }
@@ -16,3 +25,6 @@ const styles = StyleSheet.create({
     input : { borderColor : "black" , borderWidth : 1 , padding : 5 , borderRadius : 5 ,
             marginVertical : 10}
 })
+
+//<input type="text" onchange="">
+// <button onclick=""></button>
