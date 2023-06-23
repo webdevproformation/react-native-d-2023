dddfb512cc32ef06da2685608ef6e74fdb357c79
Malik H <malik.h@webdevpro.net>
2023-06-23 16:16:07 +0200

    requete ajax dans recherche

--- jour5-api/composants/Recherche.jsx
+++ jour5-api/composants/Recherche.jsx
@@ -1,16 +1,22 @@
 import { StyleSheet, Text, View  } from 'react-native'
-import React from 'react'
+import React , { useState , useEffect } from 'react'
 import FormulaireRecherche from './FormulaireRecherche'
 import Resultat from './Resultat'
 
 export default function Recherche() {
+    const [cocktails , setCocktails] = useState([])
 
-    // https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita
+    useEffect( function(){
+        fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
+            .then(function(reponse){ return reponse.json()})
+            .then(function(data){ setCocktails(data.drinks)})
+    }, [])
+
   return (
     <View>
       <Text style={{ fontSize : 25 }}>Rechercher un cocktail :</Text>
       <FormulaireRecherche />
-      <Resultat />
+      <Resultat cocktails={cocktails}/>
     </View>
   )
 }
--- jour5-api/composants/Resultat.jsx
+++ jour5-api/composants/Resultat.jsx
@@ -1,7 +1,7 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-export default function Resultat() {
+export default function Resultat({cocktails}) {
   return (
     <View>
       <Text>liste des cocktails disponibles :</Text>
