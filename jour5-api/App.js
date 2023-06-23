71f8f0e0a94daf54b5854ba5b33179fcd8988f19
Malik H <malik.h@webdevpro.net>
2023-06-23 15:01:59 +0200

    correction

--- jour5-api/App.js
+++ jour5-api/App.js
@@ -2,12 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View ,StatusBar as S } from 'react-native';
 import CycleVie from './composants/CycleVie';
 import ApiGameOfThrone from './composants/ApiGameOfThrone';
+import Exo1 from './composants/Exo1';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <CycleVie />
       <ApiGameOfThrone />
+      <Exo1 />
       <StatusBar style="auto" />
     </View>
   );
