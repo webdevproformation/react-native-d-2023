a98d8d98c24b308567c1838a5ca7b78d0d8cad3c
Malik H <malik.h@webdevpro.net>
2023-06-28 11:40:39 +0200

    ajoute pour drawer

--- jour8-firebase/App.js
+++ jour8-firebase/App.js
@@ -1,12 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native"
+import { createDrawerNavigator } from "@react-navigation/drawer"
+
 import AjouterArticle from './screens/AjouterArticle';
+import ListeArticle from './screens/ListeArticle';
+
+const Drawer = createDrawerNavigator()
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <AjouterArticle />
+      <NavigationContainer>
+        <Drawer.Navigator>
+          <Drawer.Screen name={'ajouter-article'} component={AjouterArticle} />
+          <Drawer.Screen name={'liste-article'} component={ListeArticle} />
+        </Drawer.Navigator>
+      </NavigationContainer>
+      
       <StatusBar style="auto" />
     </View>
   );
